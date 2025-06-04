df12ea23219eac74e35ddbc89ec862414f455d39
Vincent-Lin-UF <vincentlin1617@gmail.com>
2025-06-04 02:33:18 -0400

    learned POST and GET in Go/Gin

--- main.go
+++ main.go
@@ -2,14 +2,58 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+var articles = map[int]Article{}
+var nextID = 1
+
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello, Gin!")
+
+	// Create
+	router.POST("/articles", func(c *gin.Context) {
+		var newArticle Article
+		if err := c.ShouldBindJSON(&newArticle); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		newArticle.ID = nextID
+		nextID++
+		articles[newArticle.ID] = newArticle
+		c.JSON(http.StatusCreated, newArticle)
 	})
+
+	// Listing all the articles
+	router.GET("articles", func(c *gin.Context) {
+		list := make([]Article, 0, len(articles))
+		for _, a := range articles {
+			list = append(list, a)
+		}
+		c.JSON(http.StatusOK, list)
+	})
+
+	// Fetching a single article by ID
+	router.GET("/articles/:id", func(c *gin.Context) {
+		// 1. Parse ID from URL
+		idParam := c.Param("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "article not found"})
+			return
+		}
+
+		// 2. Look up in the in-memory map
+		article, exists := articles[id]
+		if !exists {
+			c.JSON(http.StatusNotFound, gin.H{"error": "article not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, article)
+	})
+
 	router.Run(":8080")
 }
